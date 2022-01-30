815a09415cb3d3cbddb87d22798714c9f65daace
mt-andrea <mateandicica@gmail.com>
2022-01-30 18:27:14 +0100

    map

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,24 +7,19 @@ function App() {
   return (
     <div>
       <h1 className="heading">Ismerősök</h1>
-      <Avatar img="https://scontent.fbud6-4.fna.fbcdn.net/v/t39.30808-6/217345019_2168017310004916_2229293593352826180_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=174925&_nc_ohc=yicHwFawAuwAX-Ujp-U&_nc_ht=scontent.fbud6-4.fna&oh=00_AT-HKyk2ej5d_s3ijxfMarOwWLvQ9CAxZHzQ7pAxSrqO2w&oe=61FB08B6"/>
-      <Card
-      name={contacts[0].name}
-      img={contacts[0].imgURL}
-      tel={contacts[0].phone}
-      email={contacts[0].email} />
-      <Card
-      name={contacts[1].name}
-      img={contacts[1].imgURL}
-      tel={contacts[1].phone}
-      email={contacts[1].email} />
-      <Card
-      name={contacts[2].name}
-      img={contacts[2].imgURL}
-      tel={contacts[2].phone}
-      email={contacts[2].email} />
+      <Avatar img="https://scontent.fbud6-4.fna.fbcdn.net/v/t39.30808-6/217345019_2168017310004916_2229293593352826180_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=174925&_nc_ohc=yicHwFawAuwAX-Ujp-U&_nc_ht=scontent.fbud6-4.fna&oh=00_AT-HKyk2ej5d_s3ijxfMarOwWLvQ9CAxZHzQ7pAxSrqO2w&oe=61FB08B6" />
+      {contacts.map(contact =>
+      (<Card
+        key={contact.id}
+        name={contact.name}
+        img={contact.imgURL}
+        tel={contact.phone}
+        email={contact.email} />)
+      )}
     </div>
   );
 }
+
+
 
 export default App;
